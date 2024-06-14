22a5493e55ec14e5493401a440512b1cfacb8516
Revansiddh Kale <rpkale.agpit@gmail.com>
2024-06-14 15:52:12 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";
+// alert(3 +3); we are using node js
+
+let name = "revan";
+let age = 18;
+let isLoggedIn = false;
+
+let wather = null
+
+// data types -->
+// number
+// biginit
+// string =>
+// boolean => true/false;
+// null => standalone value
+// undefine 
+
+console.log(typeof undefine); // undefine
+console.log(typeof null); // object
