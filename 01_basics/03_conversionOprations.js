503cf496f436ac7d1d0681c60b0a79cf35c5e9d6
Revansiddh Kale <rpkale.agpit@gmail.com>
2024-06-14 18:22:10 +0000

    oprations

--- 01_basics/03_conversionOprations.js
+++ 01_basics/03_conversionOprations.js
@@ -27,4 +27,54 @@ let booleanIsLoggedIn = Boolean(isLoggedIn); // boolean data type
 let someNumber = 33;
 
 let stringNumber = String(someNumber);  // string data type
-console.log(typeof stringNumber + ' ' + stringNumber);
+// console.log(typeof stringNumber + ' ' + stringNumber);
+
+// --------------------------------------oprations-----------------------------------------
+
+let value = 3;
+
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+
+let str1 = "revan";
+let str2 = " kale";
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(1+"2"+1);
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100;
+
+++gameCounter; // perifix 
+//gameCounter++; // postfix
+// console.log(gameCounter);
+
+// example----
+let x =3;
+let y = x++;
+// console.log(x);
+
+let a =3;
+let b = ++a;
+
+// console.log(a);
+
+
+
+
