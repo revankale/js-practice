ad1835528fefe50e1995b34dfe6634f144e9ba0b
Revansiddh Kale <rpkale.agpit@gmail.com>
2024-06-14 16:33:46 +0000

    conversion opration

---
+++ 01_basics/03_conversionOprations.js
@@ -0,0 +1,30 @@
+let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueNumber = Number(score); // number data type
+
+// console.log(valueNumber);
+
+// "33" => 33
+// "33asd" => NaN -- not a number
+// true => 1 or false => 0
+// null => 0
+// undefined => NaN
+
+
+let isLoggedIn = "revan";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn); // boolean data type
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true and 0 => false
+// "" => false
+// "revan" => true;
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);  // string data type
+console.log(typeof stringNumber + ' ' + stringNumber);
