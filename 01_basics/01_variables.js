7cb479b9363ac53469eb6cb87c2a1e1de7864c23
Revansiddh Kale <rpkale.agpit@gmail.com>
2024-06-14 14:07:44 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 14333;
+let accountEmail = "revankale@gmail.com";
+var accountPassword = "1234";
+
+accountCity = "jaipur";
+
+let accountState;
+
+/*
+prefer not to use var
+Cz issue of block scope and function scope
+*/
+
+// accountId = 2;
+
+accountEmail = "urmila@gmail.com"
+accountPassword = "09876554"
+accountCity = "pune"
+
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+
