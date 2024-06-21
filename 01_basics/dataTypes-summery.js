8b7dbe45aa86e8091752ce73864fc5cf3aa72aec
Revansiddh Kale <rpkale.agpit@gmail.com>
2024-06-21 04:05:20 +0000

    data type summery

---
+++ 01_basics/dataTypes-summery.js
@@ -0,0 +1,37 @@
+// primitive data type - string, number, boolean, null, undefined, symbol, BigInt...etc
+
+const score = 100;
+const scoreValue = 103.3;
+
+const isLoggedIn = false;
+
+const outsideTemp = null;
+
+let userEmail;
+
+const id = Symbol('123');
+
+const anotherId = Symbol('123');
+
+console.log(id === anotherId );
+
+// const bigNumber = 124848435835n
+
+
+
+// Referance (non-primitive) data type - Array, Object, function
+
+
+const hero = ["shaktiman", "nagraj", "dogo"];
+
+let myObj = {
+    name: "revan",
+    age: 28
+
+}
+
+const myFuction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof scoreValue);
