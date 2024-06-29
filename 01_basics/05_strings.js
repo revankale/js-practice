c4b10dc21269ce03ce02fb67798262299aa67ee0
Revansiddh Kale <rpkale.agpit@gmail.com>
2024-06-29 12:38:43 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "revansiddh";
+const repoCount = 50;
+
+// console.log(name + repoCount + " value");
+
+// console.log(`my name is ${name} and my repo num is ${repoCount}`);
+
+const gameName = new String('revansiddh-kale-hc');
+
+// console.log(gameName[0]);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('n'));
+
+const newString = gameName.substring(0, 4);
+
+// console.log(newString);
+
+const anotherString = gameName.slice(1, 4);
+// console.log(anotherString);
+
+const newStringOne = "   revansiddh   ";
+
+// console.log(newStringOne);
+
+// console.log(newStringOne.trim());
+
+const url = "http://revansiddh.com/revansiddh%20kale";
+// console.log(url.replace('%20', '-'));
+console.log(gameName.split('-'));
