11a0e63eabf063f6c3665bc07e19a78992d17a77
Revansiddh Kale <rpkale.agpit@gmail.com>
2024-06-30 05:55:46 +0000

    maths and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+const score = 400;
+
+// console.log(score);
+const balance = new Number(100);
+
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherValue = 124.899;
+
+// console.log(otherValue.toPrecision(4));
+
+const hundred = 1000000;
+
+// console.log(hundred.toLocaleString('en-IN'));
+
+//--------------------------------------------------------------------------------------------------//
+
+// ******************* MATHS ************************//
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(4.6));
+
+console.log(Math.min(1,3,5,7,8,3,5));
+console.log(Math.max(1,3,5,7,8,3,5));
+
+
+
+
+
+
